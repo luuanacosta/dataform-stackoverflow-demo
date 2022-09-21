433d2eae67c2213b995ae3ac22a4c86149a4f90c
André Sousa <andresousa@google.com>
2022-09-21 05:27:54 +0000

    fix

--- deploy.sh
+++ deploy.sh
@@ -1,9 +1,5 @@
 #!/bin/bash
 
 dataform install
-<<<<<<< HEAD
 echo '{"projectId": "andresousa--xyz-experimental", "location": "US"}' > .df-credentials.json
-=======
-echo '{"projectId": "andresousa--xyz-experimental", "location": "EU"}' > .df-credentials.json
->>>>>>> refs/heads/stackoverflow-workspace
 dataform run

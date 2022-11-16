8e10aeaae2eb33829f76657a9d4b462b7ab4b845
André Sousa <andresousa@google.com>
2022-11-16 00:27:20 +0000

    Fixing dataform dataset

--- deploy.sh
+++ deploy.sh
@@ -1,5 +1,5 @@
 #!/bin/bash
 
 dataform install
-echo '{"projectId": "andresousa--xyz-experimental", "location": "US"}' > .df-credentials.json
+echo '{"projectId": "andresousa-c6bank", "location": "US"}' > .df-credentials.json
 dataform run
